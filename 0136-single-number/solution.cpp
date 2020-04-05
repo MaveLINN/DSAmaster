bc460902bfb4d627c36ce36f46b2a86a67dbab26
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2020-04-05 08:50:44 +0000

    Sync LeetCode submission Runtime - 24 ms (5.60%), Memory - 9.1 MB (99.95%)

---
+++ 0136-single-number/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        unordered_set<int> s ; 
+        for(auto i : nums ){
+            if(s.count(i))s.erase(i);
+            else s.insert(i);
+        }
+        return *s.begin();
+    }
+};
