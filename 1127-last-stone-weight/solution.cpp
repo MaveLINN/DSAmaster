e06962384ec18caffa881fa0583ea928488811e0
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2020-04-22 10:56:39 +0000

    Sync LeetCode submission Runtime - 4 ms (0.43%), Memory - 6.1 MB (100.00%)

---
+++ 1127-last-stone-weight/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        
+        const int n = stones.size() ;
+        
+        priority_queue < int > pq ; 
+        for(auto i : stones){
+            pq.push(i);
+        }
+        
+while(pq.size() > 1){
+   int top1 = pq.top() ; pq.pop(); 
+   int top2 = pq.top() ; pq.pop(); 
+    
+    pq.push( abs(top1 - top2));
+}
+        
+        return pq.top();
+    }
+};
