cd8289a2ab1ab218e15c5c840556c55e5d67f778
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2021-05-09 09:38:17 +0000

    Sync LeetCode submission Runtime - 4 ms (0.67%), Memory - 8.8 MB (100.00%)

---
+++ 1528-kids-with-the-greatest-number-of-candies/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int n = *max_element(candies.begin() , candies.end());
+        
+        vector<bool> v ; 
+        for(auto i : candies){
+            if( i + extraCandies >= n)v.push_back(true);
+            else v.push_back(false);
+        }
+        
+        return v ;
+    }
+};
