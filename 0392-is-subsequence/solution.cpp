a184b91180f46016910b9bed8f958fd6658a08e9
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2021-05-20 13:06:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.2 MB (100.00%)

---
+++ 0392-is-subsequence/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        const int n = s.size();
+        const int m = t.size();
+        
+        int k = 0 ;
+        for(int i = 0 ; i < m ; i++){
+            if(s[k] == t[i])k++;
+        }
+        
+        if(k == n)return true ;
+        
+        return false ;
+    }
+};
