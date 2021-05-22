10b5f2517cf8683d9808c0e1f57f0fdba911b69e
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2021-05-22 15:41:09 +0000

    Sync LeetCode submission Runtime - 4 ms (0.85%), Memory - 8.1 MB (100.00%)

---
+++ 1848-sum-of-unique-elements/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        
+        const int n = nums.size();
+        int res = 0 ; 
+         
+        unordered_map<int , int > mp ; 
+        
+        for(auto i : nums)
+            mp[i]++;
+        
+        for(auto i : mp)
+            if(i.second == 1) res += i.first ;
+        
+        return res ; 
+    }
+};
