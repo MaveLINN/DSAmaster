7f164b6b390f1ec444f6cac40837c8e58839ed83
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2021-05-28 19:03:17 +0000

    Sync LeetCode submission Runtime - 8 ms (4.27%), Memory - 15.4 MB (100.00%)

---
+++ 0944-smallest-range-i/solution.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    int smallestRangeI(vector<int>& nums, int k) {
+        return *max_element(nums.begin() , nums.end()) - *min_element(nums.begin() , nums.end())  <= 2*k ? 0 : *max_element(nums.begin() , nums.end()) - *min_element(nums.begin() , nums.end()) - 2*k ;
+    }
+};
