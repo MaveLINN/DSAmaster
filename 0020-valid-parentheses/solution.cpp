2e532d48f221accb2f350ba22fb1791ea394cc28
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2021-06-05 17:30:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.4 MB (100.00%)

--- 0020-valid-parentheses/solution.cpp
+++ 0020-valid-parentheses/solution.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
     bool isValid(string s) {
-    stack<char>st;
-    for(auto i : s){
-	if( i == '(' || i == '{' || i =='['){
-		st.push(i);
-		continue ;
-	}
-
-	if(i == ')' && !st.empty() && st.top() == '('){
-		st.pop();
-		continue;
-	}
-
-
-	if(i == '}' && !st.empty() && st.top() == '{'){
-		st.pop();
-		continue;
-	}
-
-	if(i == ']' && !st.empty() && st.top() == '['){
-		st.pop();
-		continue;
-	}
-
-	if( i == ')' || i == '}' || i ==']'){
-		st.push(i);
-		break ;
-	}
-}
-        return st.empty();
-
+        const int n = s.size();
+        stack<int>  st ;
+        char x ;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(s[i] == '{' || s[i] == '(' || s[i] == '['){
+                st.push(s[i]);
+                continue;
+            }
+            
+            if(st.empty()) return false ;
+            
+            switch(s[i]){
+                    
+                case '}' :
+                    x = st.top() ; st.pop();
+                    if(x == '(' || x =='[') return false ;
+                    break;
+                    
+                case ')' :
+                    x = st.top() ; st.pop();
+                    if(x == '{' || x =='[') return false ;
+                    break;
+                    
+                case ']' :
+                    x = st.top() ; st.pop();
+                    if(x == '{' || x =='(') return false ;
+                    break;
+                    
+            }
+        }
+        
+        return st.empty() ;
+        
     }
 };
