84fb08dcb30aa5043c4cb598adeafc15c0a354e7
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2021-06-06 17:38:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.1 MB (100.00%)

---
+++ 0742-to-lower-case/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        const int n = s.size();
+        
+        for(int i = 0 ; i < n ;i++){
+            if(s[i] > 64 and s[i] < 91){
+                s[i] = s[i] + 32 ;
+            }
+        }
+        return s;
+    }
+};
