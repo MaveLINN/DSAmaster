f6f4cc2e2154bc32bbc5002d5df6c6e856d7bc10
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2021-06-11 20:51:06 +0000

    Sync LeetCode submission Runtime - 20 ms (5.80%), Memory - 7.7 MB (100.00%)

---
+++ 0013-roman-to-integer/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        
+    unordered_map<char, int> T = { { 'I' , 1 },
+                                   { 'V' , 5 },
+                                   { 'X' , 10 },
+                                   { 'L' , 50 },
+                                   { 'C' , 100 },
+                                   { 'D' , 500 },
+                                   { 'M' , 1000 } };
+                                   
+   int sum = T[s.back()];
+   for (int i = s.length() - 2; i >= 0; --i) 
+   {
+//        cout << T[s[i]] << " g " <<  T[s[i+1]] << endl ;
+//        if (T[s[i]] < T[s[i + 1]])
+//        {
+//            sum -= T[s[i]];
+//        }
+//        else
+//        {
+//            sum += T[s[i]];
+//        }
+       
+//        cout << sum << endl ;
+       
+       if(T[s[i]] >= T[s[i+1]]) sum += T[s[i]];
+       else sum -= T[s[i]] ;
+       
+   }
+    
+   return sum;
+    }
+};
