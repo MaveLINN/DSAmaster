c0bc48182ea5f38b7d6c3f6071995737948fe037
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2021-06-30 20:53:06 +0000

    Sync LeetCode submission Runtime - 8 ms (10.10%), Memory - 6 MB (100.00%)

---
+++ 0009-palindrome-number/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0)return false;
+        string s = to_string(x);
+        
+        const int n = s.size();
+        for(int i = 0 ; i < n/2 ; i++){
+            if(s[i] != s[n-i-1])return false ;
+        }
+        
+        return true ;
+    }
+};
