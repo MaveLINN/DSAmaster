08594ee3ebfaa4466a5d5116621e545b4a9c7a6b
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2021-07-27 20:28:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.2 MB (100.00%)

---
+++ 1490-generate-a-string-with-characters-that-have-odd-counts/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        string res="";
+        if(n%2){
+        for(int i = 0 ; i < n ; i++ ) res += 'a';
+            return res ;
+        }
+        else {
+                    for(int i = 0 ; i < n-1 ; i++ ) res += 'a';
+            res+='b';
+            return res ;
+        }
+    }
+};
