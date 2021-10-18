789af337b14d95f3bd377ca588790f4a0dda43be
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2021-10-18 09:00:27 +0000

    Sync LeetCode submission Runtime - 4 ms (60.12%), Memory - 10.7 MB (100.00%)

--- 1482-how-many-numbers-are-smaller-than-the-current-number/solution.cpp
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/solution.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
-        const int n = nums.size();
-        
-        int gg = *max_element(nums.begin() , nums.end());
-        int has[gg+1] , pre[gg+1];
-        
-        memset(has , 0 , sizeof has);
-        memset(pre , 0 , sizeof pre);
-        
+        map<int,int> mp; 
         for(auto i : nums){
-            has[i]++;
-        }
-        int sum = 0 ;
-        for(int i = 0 ; i < gg+1 ; i++){
-            
-            if(has[i]){
-                pre[i] = sum ;
-                sum += has[i] ;
-            }
-            
-        }
-        vector<int > v ;
-        
-        for(int i = 0 ; i < n; i++){
-            v.push_back(pre[nums[i]]);
+            mp[i]++;
         }
         
-        return v ;
+        int prev = 0 ;
+        for(auto &i : mp){
+            int n = i.second ;
+            i.second = prev ;
+            prev += n ;
+        }
+        
+        vector<int> res ;
+        for(auto i : nums){
+            res.push_back(mp[i]);
+        }
+        
+        return res ;
     }
 };
