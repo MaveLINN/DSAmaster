986b45311062d3f07445bcc957a4cea86ac76242
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2021-10-18 07:01:42 +0000

    Sync LeetCode submission Runtime - 8 ms (0.30%), Memory - 7.9 MB (100.00%)

--- 1791-richest-customer-wealth/solution.cpp
+++ 1791-richest-customer-wealth/solution.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        const int n = accounts.size();
-        const int m = accounts[0].size();
-        int res = 0 ;
         
-        
+        int n = accounts.size() , res = 0 ;
         for(int i = 0 ; i < n ; i++){
-            res = max(res , accumulate(accounts[i].begin() , accounts[i].end(),0));
+            res = max(res,accumulate(accounts[i].begin(),accounts[i].end(),0));
         }
-        
         return res ;
     }
 };
