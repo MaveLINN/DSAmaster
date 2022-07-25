945006f6f04441ab341484948b8316d522ac8489
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2022-07-25 10:11:27 +0000

    Sync LeetCode submission Runtime - 22 ms (36.15%), Memory - 11 MB (99.99%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -1,27 +1,33 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-    const int n = nums.size();
-    vector < int > ret(2);
         
-        int k = 0 ;
-        vector<pair<int,int>>vp ;   
-        for(auto i : nums)vp.push_back({i , k++});
+        int num = 0 ;
+        vector<int> v;
+        vector<pair<int,int>> vp ; 
         
-        sort(vp.begin() , vp.end()) ;
-        int l = 0 , h = n - 1 , m ; 
-        while( l < h ){
-            m = vp[l].first + vp[h].first ;
-            if( m == target){
-                ret[0] = vp[l].second ; 
-                ret[1] = vp[h].second ;
+        for(auto &i : nums){
+            vp.push_back({i,num++});
+        }
+        
+        sort(vp.begin() , vp.end());
+
+        int l = 0 , h = nums.size() - 1;
+        while(l < h){
+            int sum = vp[l].first + vp[h].first ;
+
+            if( sum == target){
+                v.push_back(vp[l].second);
+                v.push_back(vp[h].second);
                 break ;
             }
-           if( m > target) h-- ;
-           else l++ ;
+            else{
+                if(sum > target) h-- ;
+                else l++ ;
+            }
+        
         }
-            
-        return ret ;
-
+        
+        return v ;
     }
 };
