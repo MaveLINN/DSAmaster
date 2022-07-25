5ddf0c8ce75b58d41832f23f418de7f3bce63c2b
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2022-07-25 07:25:02 +0000

    Sync LeetCode submission Runtime - 7 ms (19.22%), Memory - 7.3 MB (100.00%)

--- 0242-valid-anagram/solution.cpp
+++ 0242-valid-anagram/solution.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-
+     
         vector<int> v(26) ;
-        if(s.size() != t.size())return false ; 
-        for(int i = 0 ; i < s.size() ; i++) v[s[i]-97]++ , v[t[i]-97]-- ;
-        for(auto i : v)if(i != 0)return false ;
-        return true ;    
+        
+        for(auto &i : s){
+            v[i-97]++ ;
+        }
+        
+        for(auto &i : t){
+            v[i-97]-- ;
+        }
+        
+        for(int i = 0 ; i < 26 ; i++){
+            if(v[i]) return false ;
+        }
+        return true ;
     }
 };
