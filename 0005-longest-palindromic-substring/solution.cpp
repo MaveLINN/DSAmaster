a2d0e9abfd39d2fef3dc14d2c0e5bab5f5d2178e
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2022-09-20 00:38:41 +0000

    Sync LeetCode submission Runtime - 200 ms (19.51%), Memory - 159.6 MB (22.55%)

--- 0005-longest-palindromic-substring/solution.cpp
+++ 0005-longest-palindromic-substring/solution.cpp
@@ -2,32 +2,33 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         
-        
-        int st = 0 , ed = 0 , res = 0 , gg = 0 ;
+        int start = 0, res = 0 , stringSize = 0 ;
         
         for(int i = 0 ; i < s.size() ; i++){
-        int x = palen( s , i , i);
-        int xx = palen(s , i , i + 1);    
-        res = max(x , xx);
+        int x = palen( s , i , i); // odd palindrome start
+        int xx = palen(s , i , i + 1);    // even palindrome start
+        res = max(x , xx); // finding the maximum length of palindrome string
             
-          if(res > (ed - st)){
-              gg = res ;
-              st = i - (res - 1) / 2 ;
-              ed = i + (res / 2) ;
+          if(res > stringSize ){
+              stringSize = res ;
+              start = i - (res - 1) / 2 ;
           }  
+            
         }
         
-        string ans = s.substr( st , gg);
+        string ans = s.substr( start , stringSize);
         return ans ;
     }
     
     
     
     int palen(string s , int l , int r ){
-        if(s == "" or l > r)return 0 ; 
-        while( l >= 0 and r < s.size() and s[l] == s[r]){
-            l-- ; r++;
+        if(s == "" or l > r)return 0 ;  
+        
+        while( l >= 0 and r < s.size() and s[l] == s[r]){ // condition for the palindrome in 
+            l-- ; r++;                                    // in string .
         }
-        return r-l-1;
+        
+        return r-l-1; // returning the length of the palindrome string 
     }
 };
