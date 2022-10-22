884cb815cd6911e377ea82db39eed1a41383bc6e
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2022-10-22 19:56:49 +0000

    Sync LeetCode submission Runtime - 43 ms (24.22%), Memory - 13.7 MB (99.99%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -2,32 +2,16 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        int num = 0 ;
-        vector<int> v;
-        vector<pair<int,int>> vp ; 
-        
-        for(auto &i : nums){
-            vp.push_back({i,num++});
-        }
-        
-        sort(vp.begin() , vp.end());
-
-        int l = 0 , h = nums.size() - 1;
-        while(l < h){
-            int sum = vp[l].first + vp[h].first ;
-
-            if( sum == target){
-                v.push_back(vp[l].second);
-                v.push_back(vp[h].second);
-                break ;
+        vector<int>res ;
+        unordered_map<int,int> mp ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(mp[target-nums[i]]){
+                res.push_back(i);
+                res.push_back(mp[target-nums[i]]-1);
             }
-            else{
-                if(sum > target) h-- ;
-                else l++ ;
-            }
-        
+
+            mp[nums[i]] = i+1 ;
         }
-        
-        return v ;
+        return res ;
     }
 };
