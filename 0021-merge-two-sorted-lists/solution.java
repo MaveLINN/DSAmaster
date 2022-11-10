234e8c5cf22252037d65ed6b85edef597ab47896
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2022-11-10 17:19:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.7 MB (100.00%)

--- 0021-merge-two-sorted-lists/solution.java
+++ 0021-merge-two-sorted-lists/solution.java
@@ -10,22 +10,24 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode res = new ListNode(6) ;
-        ListNode temp = res;
-        while(list1 != null && list2 != null){
-            if(list1.val < list2.val){
-                res.next = list1 ;
-                list1 = list1.next ;
-            }
-            else{
-                res.next = list2 ;
+        ListNode temp = new ListNode(1); // a dummy node which help in pointing the sorted nodes ,
+        ListNode res = temp ; // a pointer which points the initial poistion of dummy node .
+
+        while(list1 != null && list2 != null ){ // if any of  the list is null then it should be assigned directly
+            if(list1.val > list2.val){      
+                temp.next = list2;
                 list2 = list2.next ;
             }
-            res = res.next ;
+            else{
+                temp.next = list1;
+                list1 = list1.next ;
+            }
+            temp = temp.next ;
         }
+        temp.next = list1 == null ? list2 : list1 ; // if list is null 
 
-        res.next = list1 == null ? list2 : list1 ;
 
-        return temp.next ;
+        return res.next ; // as res point to the initial position of temp , and the resultant list starts from the temp next .         
+                          
     }
 }
