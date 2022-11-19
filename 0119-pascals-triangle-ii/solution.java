1e4bf67ab5fcf0720d2db348a0fdcbfd4e3bd0b0
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2022-11-19 14:27:34 +0000

    Sync LeetCode submission Runtime - 2 ms (24.76%), Memory - 41.9 MB (99.42%)

---
+++ 0119-pascals-triangle-ii/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<Integer> list = new ArrayList<Integer>();
+        for(int i = 0 ; i < rowIndex + 1 ; i++){
+            list.add(0,1); // each time adding 1 in front side and other elements were shifted
+            for(int j = 1 ; j < i ; j++){
+                list.set(j , list.get(j) + list.get(j+1));
+            }
+            res.add( new ArrayList<Integer>(list)); // stores the current list value . 
+        }
+        return res.get(rowIndex) ;
+        
+    }
+}
