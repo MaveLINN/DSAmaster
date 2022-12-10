7a15f8682bfdce89de548349039e551ee89ce8a1
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2022-12-10 12:23:09 +0000

    Sync LeetCode submission Runtime - 6 ms (1.36%), Memory - 83.9 MB (99.85%)

--- 0121-best-time-to-buy-and-sell-stock/solution.java
+++ 0121-best-time-to-buy-and-sell-stock/solution.java
@@ -5,8 +5,8 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         list.add(-1);
         for(int i = prices.length - 2 ; i>= 0 ; i--){
-            rightMax = Math.max(rightMax,prices[i+1]);
-            res = Math.max(rightMax-prices[i] , res);
+            rightMax = Math.max(rightMax,prices[i+1]); // finding max in right side excluding current value
+            res = Math.max(rightMax-prices[i] , res);  // FINDING max result on spot and looping on . 
         }
         return res ;
     }
