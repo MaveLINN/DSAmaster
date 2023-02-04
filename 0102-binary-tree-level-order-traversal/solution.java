917761cbabc25bf08a0719ea4ebd8381efc0e11d
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2023-02-04 19:47:43 +0000

    Sync LeetCode submission Runtime - 1 ms (96.24%), Memory - 42 MB (100.00%)

--- 0102-binary-tree-level-order-traversal/solution.java
+++ 0102-binary-tree-level-order-traversal/solution.java
@@ -14,26 +14,36 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode node) {
-        List<List<Integer>> list = new LinkedList<List<Integer>>();
-        if(node == null) return list;
-        
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        q.offer(node);
-        
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List< List<Integer> > list = new ArrayList< List<Integer> >();
+
+        if(root == null)
+        return list ;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
+        q.offer(root); // initial push in queue
+        q.offer(null); // this null will tell to add the list to list Of list .. like  3 N 9 20 N 15 7 N 
+
         while(!q.isEmpty()){
-            
-            int n = q.size();
-            List<Integer> l = new LinkedList<Integer>() ;
-            
-            for(int i = 0 ; i < n ; i++){
-            TreeNode k = q.peek(); q.poll();
-            l.add(k.val);
-            if(k.left != null)q.offer(k.left);
-            if(k.right != null)q.offer(k.right);
+            TreeNode node = q.poll();
+
+            if(node == null){ // it means to change the new line 
+                list.add(new ArrayList(ans)); // adding the existing list to result .
+                ans.clear(); 
+                                  
+                if(q.isEmpty()) // if empty means there is no node after that .
+                    break ;
+                else q.offer(null); // if value exist after null , then we need to insert the null again   
             }
-            list.add(l);
+            else{
+                ans.add(node.val);
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null)q.offer(node.right);
+            }
+
         }
-        return list;
+        return list ;
     }
 }
