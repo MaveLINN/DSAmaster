9958351504d9f155f048c1369dbc7658d496d5ab
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2023-08-20 00:26:27 +0000

    Sync LeetCode submission Runtime - 4 ms (18.17%), Memory - 44.1 MB (99.90%)

---
+++ 0090-subsets-ii/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        solve(nums,0,res,list) ;
+        return res.stream().distinct().collect(Collectors.toList());
+    }
+
+    public void solve(int[] a , int i , List<List<Integer>> res , List<Integer> list){
+        if(i == a.length ){
+           List<Integer>check = new ArrayList<>(list);
+           Collections.sort(check);
+            res.add(new ArrayList<>(check)) ;
+            check.clear();
+            return;
+        }
+
+        list.add(a[i]);    
+        solve(a,i+1,res,list);
+        list.remove(list.size()-1);
+        solve(a,i+1,res,list);
+
+    }
+}
