e1331f7139c1a0f8353d930c514219042759d825
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2023-10-20 10:25:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 39.2 MB (100.00%)

--- 0070-climbing-stairs/solution.java
+++ 0070-climbing-stairs/solution.java
@@ -1,13 +1,10 @@
 class Solution {
-    int[] dp = new int[100]; // dp for storing the answer , this is for not to calculating the answer again and again
-
+    int dp[] = new int[50] ;
     public int climbStairs(int n) {
-        if(n == 1 || n == 2) return n ; // think for a base condition when n = 1 or n = 2 ,
+        
+        if(n == 1 || n == 2) return n ;
+        if(dp[n] != 0) return dp[n];
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2) ;
 
-        if(dp[n] != 0) return dp[n]; // if the answer for that sub problem already exists , then why to find again
-        
-        dp[n] = climbStairs(n - 1 ) + climbStairs(n-2) ; // if we didn't got the answer in dp array then we need to find that , follow the number of ways to call the function and add the result .
-        
-        return dp[n] ; //  return the answer for the required n .
     }
 }
