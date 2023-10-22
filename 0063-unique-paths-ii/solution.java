dcc7f1d5f110083f0d01011debb7f126b3fd77f7
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2023-10-22 22:59:21 +0000

    Sync LeetCode submission Runtime - 2 ms (3.91%), Memory - 43.1 MB (95.74%)

--- 0063-unique-paths-ii/solution.java
+++ 0063-unique-paths-ii/solution.java
@@ -1,22 +1,22 @@
 class Solution {
-    int[][] dp = new int[101][101] ;
+    int[][] dp = new int[101][101];
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int n = obstacleGrid.length;
-        if(n == 0) return 0 ;
-        int m = obstacleGrid[0].length;
-
+        int n = obstacleGrid.length ;
+        int m = obstacleGrid[0].length ;
+        
         for(int i = 0 ; i < 101 ; i++){
             for(int j = 0 ; j < 101 ; j++){
                 dp[i][j] = -1 ;
             }
         }
-        return solve(obstacleGrid,0,0,n - 1,m - 1);
+        return solve(obstacleGrid,n-1,m-1);
     }
 
-    public int solve(int[][] a , int i , int j , int n , int m){
-        if(i > n || j > m || a[i][j] == 1) return 0 ;
-        if(i == n && j == m) return 1 ;
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = solve(a,i+1,j,n,m) + solve(a,i,j+1,n,m);
+    int solve(int[][] a , int n , int m ){
+
+            if(m < 0 || n < 0 || a[n][m] == 1 ) return 0;
+            if( n == 0 && m == 0) return 1 ;
+            if(dp[n][m] != -1 ) return dp[n][m] ; 
+            return dp[n][m] = solve(a,n,m-1) + solve(a,n-1,m);
     }
 }
