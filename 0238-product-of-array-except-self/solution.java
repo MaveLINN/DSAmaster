28207ba30f40d2a1d2fb6fd894a6e63a9f571fa4
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2024-09-07 22:32:03 +0000

    Sync LeetCode submission Runtime - 5 ms (3.20%), Memory - 55.1 MB (99.93%)

--- 0238-product-of-array-except-self/solution.java
+++ 0238-product-of-array-except-self/solution.java
@@ -1,43 +1,32 @@
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length ;
-        int[] pre = new int[n+1];
-        int[] suf = new int[n+1];
-        int[] res = new int[n];
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int countZero = 0, mulValue = 1, lastZeroIndex = -1;
+        int[] pre = new int[n+1]; pre[0] = 1 ;
+        int[] suf = new int[n+1]; suf[n] = 1 ;
 
-        pre[0] = 1 ; suf[n] = 1 ;
-        for(int i = 0 ; i < n ; i++){
-            pre[i+1] = pre[i]*nums[i];
-            suf[n-1-i] = suf[n-i]*nums[n-1-i];
+        for (int i = 0; i < nums.length; i++) {
+            pre[i+1]= pre[i]*nums[i];
+            suf[n-1-i] = suf[n-i]*nums[n-i-1];
+            if (nums[i] == 0) {
+                countZero++;
+                lastZeroIndex = i;
+            } else {
+                mulValue *= nums[i];
+            }
         }
 
-        for(int i = 1 ; i <= n ; i++){
-            res[i-1] = pre[i-1]*suf[i];
+        if (countZero > 1) {
+            Arrays.fill(nums, 0);
+            return nums;
+        } else if (countZero == 1) {
+            Arrays.fill(nums, 0);
+            nums[lastZeroIndex] = mulValue;
+        } else {
+            for (int i = 0; i < n; i++) {
+                nums[i] = pre[i]*suf[i+1];
+            }
         }
-
-        return res ;
-        // int cz = 0 ;
-        // int pro = 1 ;
-        // int idx = 0 ;
-        // int[] res = new int[nums.length];
-
-        // for(int i = 0 ; i < nums.length ; i++) res[i] = 0 ;
-
-        // for(int i = 0 ; i < nums.length ; i++){
-        //     if(nums[i] != 0) pro*= nums[i] ;
-        //     else {
-        //         cz++ ; idx = i ; pro *= 1 ;
-        //     }
-        // } 
-
-        // if(cz == 0){
-        //     for(int i = 0 ; i < nums.length ; i++){
-        //         res[i] = pro/nums[i];
-        //     }
-        // }else if(cz == 1){
-        //     res[idx] = pro ;
-        // }
-
-        // return res ;
+        return nums;
     }
 }
