6080c0eaf295ae1ccc5f06b1be04fd0ece4d58e4
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2024-09-15 18:51:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.6 MB (99.98%)

--- 0070-climbing-stairs/solution.java
+++ 0070-climbing-stairs/solution.java
@@ -1,10 +1,11 @@
 class Solution {
-    int dp[] = new int[50] ;
+    int[] dp = new int[50];
     public int climbStairs(int n) {
-        
-        if(n == 1 || n == 2) return n ;
+        if(n == 0) return 1;
+        if(n < 0) return 0 ;
         if(dp[n] != 0) return dp[n];
-        return dp[n] = climbStairs(n-1) + climbStairs(n-2) ;
-
+        int x = climbStairs(n-1);
+        int xx = climbStairs(n-2);
+        return dp[n] = x + xx ;
     }
 }
