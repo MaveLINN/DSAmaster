574904fc6bdb1b04eff0376f047c0d41a359ff64
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2024-09-18 23:46:02 +0000

    Sync LeetCode submission Runtime - 31 ms (5.34%), Memory - 55.8 MB (99.92%)

--- 0217-contains-duplicate/solution.java
+++ 0217-contains-duplicate/solution.java
@@ -1,18 +1,19 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
-    Map<Integer,Integer> mp = new HashMap<>();
-    
-    for(int i = 0 ; i < nums.length ; i++){
-        if(mp.containsKey(nums[i])){ // checking if the key is already exists
-            mp.put(nums[i] , mp.get(nums[i]) + 1);
-        }
-        else mp.put(nums[i] , 1);
-    }
+        Map<Integer,Integer> mp = new HashMap<>();
+        int n = nums.length ;
 
-    for(Map.Entry<Integer,Integer> i : mp.entrySet()){
-        if(i.getValue() > 1) return true ; // if any value greater than 1 means duplicate exists .
-    }
-    return false ;    
+        for(int i = 0 ; i < n ; i++){
+            if(mp.containsKey(nums[i])){
+                mp.put(nums[i],mp.get(nums[i])+1);
+            }
+            else mp.put(nums[i],1);
+        }
+
+        for(Map.Entry<Integer,Integer> e : mp.entrySet()){
+            if(e.getValue()>1)
+            return true;
+        }
+        return false;
     }
 }
