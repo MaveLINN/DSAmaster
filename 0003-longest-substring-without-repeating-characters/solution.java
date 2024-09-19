8b7d6d6c33b362974bc872c37cb5d2213ac8ba22
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2024-09-19 21:02:52 +0000

    Sync LeetCode submission Runtime - 2 ms (97.52%), Memory - 43.8 MB (99.89%)

--- 0003-longest-substring-without-repeating-characters/solution.java
+++ 0003-longest-substring-without-repeating-characters/solution.java
@@ -1,20 +1,15 @@
 class Solution {
-   public int lengthOfLongestSubstring(String s) {
-
+    public static int lengthOfLongestSubstring(String s) {
        int[] a = new int[256];
-       for(int i = 0 ; i < 256 ; i++) a[i] = -1 ;
-       int check = -1 , result = 0 ;
-
+       for(int i = 0 ; i < 256 ; i++)a[i] = -1;
+       int check = -1 , res = 0 ;
        for(int i = 0 ; i < s.length() ; i++){
-
-           if(a[s.charAt(i)] > check)
-                check = a[s.charAt(i)] ;
+           if(a[s.charAt(i)] > check ){
+               check = a[s.charAt(i)];
+           }
            a[s.charAt(i)] = i ;
-           result = Math.max(result , i - check);
+           res = Math.max(res,i-check);
        }
-
-       return result ;
-
+        return res ;
     }
 }
-
