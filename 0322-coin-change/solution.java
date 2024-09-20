2c21c31208f221f0df230e2fbfb1aa09a1b375e4
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2024-09-20 18:30:01 +0000

    Sync LeetCode submission Runtime - 95 ms (5.05%), Memory - 48.2 MB (5.43%)

--- 0322-coin-change/solution.java
+++ 0322-coin-change/solution.java
@@ -1,20 +1,21 @@
 class Solution {
-    int[][] dp = new int[10001][15];
-
+    int[][] dp = new int[10007][15] ;
     public int coinChange(int[] coins, int amount) {
-        for(int i = 0 ; i < 10001 ; i++){
+        int n = coins.length ;
+        for(int i = 0 ; i < 10007 ; i++){
             for(int j = 0 ; j < 15 ; j++){
-                dp[i][j] = -1 ;
+                dp[i][j] = -1;
             }
         }
-        int val = solve(coins,amount,coins.length-1,0);
-        return val >= 123456 ? -1 : val ;
+        int res = solve(coins,0,amount,n-1) ;
+        return res >= 123456 ? -1 : res;
     }
 
-    public int solve(int[] a, int target , int n ,  int i){
-        if(target == 0 ) return 0 ;
-        if(i > n || target < 0 ) return 123456 ;
-        if(dp[target][i] != -1) return dp[target][i] ;
-        return dp[target][i] = Math.min( 1+solve(a,target - a[i],n,i) , solve(a,target,n,i+1) );
+    public int solve(int[] a , int idx , int target , int size){
+        if(target == 0) return 0;
+        if(target < 0 || idx > size) return 123456 ;
+
+        if(dp[target][idx] != -1) return dp[target][idx] ;
+        return dp[target][idx] = Math.min( 1 + solve(a,idx,target - a[idx],size) , solve(a,idx+1,target,size));
     }
 }
