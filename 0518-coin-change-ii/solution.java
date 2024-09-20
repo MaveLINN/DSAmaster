c3cd30e7d1e0ed8edfa9b49b8041e81ba0648d01
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2024-09-20 18:40:41 +0000

    Sync LeetCode submission Runtime - 52 ms (5.00%), Memory - 59.4 MB (5.19%)

--- 0518-coin-change-ii/solution.java
+++ 0518-coin-change-ii/solution.java
@@ -1,18 +1,20 @@
 class Solution {
-    int[][] dp = new int[301][5001];
+    int[][] dp = new int[5005][303];
     public int change(int amount, int[] coins) {
-        for(int i = 0 ; i < 301 ; i++){
-            for(int j = 0 ; j < 5001 ; j++){
-                dp[i][j] = -1 ;
+        int n = coins.length;
+        for(int i = 0 ; i < 5005 ; i++){
+            for(int j = 0 ; j < 303 ; j++){
+                dp[i][j] = -1;
             }
         }
-     return solve(coins,coins.length - 1,0,amount);   
+        return solve(coins,amount,0,n-1);
     }
 
-    int solve(int[] a , int n , int i , int target){
-        if(target == 0) return 1 ;
-        if(target < 0 || i > n) return 0 ;
-        if(dp[i][target] != -1 ) return dp[i][target] ;
-        return dp[i][target] = solve(a,n,i,target - a[i]) + solve(a,n,i+1,target) ;
+    public int solve(int[] a,int target,int idx,int size){
+        if(target == 0) return 1;
+        if(target < 0 || idx > size) return 0;
+
+        if(dp[target][idx] != -1 ) return dp[target][idx];
+        return dp[target][idx] = solve(a,target - a[idx],idx,size) + solve(a,target,idx+1,size);
     }
 }
