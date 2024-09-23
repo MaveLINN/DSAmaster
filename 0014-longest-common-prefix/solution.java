f0ec6cda1550d1bbe70f80ed2318dcc98d2cc73f
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2024-09-23 19:57:52 +0000

    Sync LeetCode submission Runtime - 10 ms (5.49%), Memory - 44.5 MB (5.37%)

--- 0014-longest-common-prefix/solution.java
+++ 0014-longest-common-prefix/solution.java
@@ -1,19 +1,22 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String res = strs[0] ;
-        for(int i = 1 ; i < strs.length ; i++){ // array length mai function bracket nhi hota
-            res = check(res,strs[i]);
+        if(strs.length == 0) return "";
+        if(strs.length == 1) return strs[0];
+        String check = strs[0];
+        int n = strs.length;
+        for(int i = 1 ; i < n ; i++){
+            check = commonPartition(check,strs[i]);
         }
-    return res ;
+        return check;
     }
 
-    String check(String  a , String  b){
-        String res = "";
-        for(int i = 0 ; i < Math.min(a.length(),b.length()) ; i++){ // string length mai function bracket hota hai 
-            if(a.charAt(i) != b.charAt(i))
-            break ;
-            res += a.charAt(i);
+    public String commonPartition(String s1 , String s2){
+        String check ="";
+        for(int i = 0 ; i < Math.min(s1.length(),s2.length()) ; i++){
+            if(s1.charAt(i) == s2.charAt(i))
+            check += s1.charAt(i);
+            else break;
         }
-        return res ;
+        return check;
     }
 }
