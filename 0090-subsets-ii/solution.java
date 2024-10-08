9dc53ef7c04e1321a448c22692bd513c0d16487a
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2024-10-08 02:04:41 +0000

    Sync LeetCode submission Runtime - 5 ms (10.08%), Memory - 44.5 MB (99.30%)

--- 0090-subsets-ii/solution.java
+++ 0090-subsets-ii/solution.java
@@ -3,7 +3,7 @@ class Solution {
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
         solve(nums,0,res,list) ;
-        return res.stream().distinct().collect(Collectors.toList());
+        return res.stream().distinct().collect(Collectors.toList());// to get the distinct list
     }
 
     public void solve(int[] a , int i , List<List<Integer>> res , List<Integer> list){
