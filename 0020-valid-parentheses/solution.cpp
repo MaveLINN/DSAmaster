5851a3359ef80791ec188f2fec16be1da67c5699
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2024-10-13 03:13:03 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8 MB (99.98%)

--- 0020-valid-parentheses/solution.cpp
+++ 0020-valid-parentheses/solution.cpp
@@ -4,18 +4,18 @@ public:
         stack<char> st ;
         for(int i = 0 ; i < s.size() ; i++)
         {
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]) ;
-            else if(st.empty()) return false ;
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') st.push(s[i]) ; // open Parentheses always push to stack
+            else if(st.empty()) return false ;   // if closed Parentheses comes and stack is empty then return false .
             else{
-                char x = st.top() ;
+                char x = st.top() ;   // closed Parentheses comes here and if the stacks top ( open Parentheses ) does not matches with its closed Parentheses then return false . 
                 if(s[i] == ']' and x != '[') return false;
                 if(s[i] == ')' and x != '(') return false ;
                 if(s[i] == '}' and x != '{') return false ;
 
-                st.pop();
+                st.pop();  // if matched the pop the top and continue .
             }
         }
 
-        return st.empty() ;
+        return st.empty() ; // if stack remains empty then the answer is true else false .
     }
 };
