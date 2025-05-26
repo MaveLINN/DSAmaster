66a15c40732dff38bab2df1c5059b060fed8a838
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2025-05-26 21:10:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 27.4 MB (17.40%)

--- 0042-trapping-rain-water/solution.cpp
+++ 0042-trapping-rain-water/solution.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    int trap(vector<int>& height) {
-        
-        const int n = height.size();
-        if(!n)return 0 ;
-        vector<int> vl(n) , vr(n) ; // left array and right array 
-        
-        vl[0] = height[0] , vr[n-1] = height[n-1];
-        for(int i = 1 ; i < n ; i++ ){
-            vl[i] = max(height[i],vl[i-1]); // finding the left max including                                                       current value 
-        }
-        
-        for(int i = n-2 ; i >= 0 ; i-- ){
-            vr[i] = max(height[i],vr[i+1]); // finding the right max including the                                                  current value 
-        }
-        
-        int result = 0 ;
-        
-        for(int i = 0 ; i < n ; i++){
-            result += abs( min(vr[i],vl[i])  - height[i]) ;
-        }
-        
-        return result ;
+    int trap(vector<int>& a) {
+    int n = a.size();
+    std::vector<int> v_leftMax(n);
+    std::vector<int> v_rightMax(n);
+
+    v_leftMax[0] = a[0];
+    v_rightMax[n-1] = a[n-1];
+
+    for(int i = 1 ; i < n ; i++){
+      v_leftMax[i] = max(v_leftMax[i-1],a[i]);
+    }
+
+    for (int i = n - 2 ; i > -1 ; --i){
+      v_rightMax[i] = max(v_rightMax[i+1],a[i]);
+    }
+    
+
+    int water_stored = 0 ;
+    for(int i = 0 ; i < n ; i++){
+      water_stored += abs( min(v_leftMax[i],v_rightMax[i]) - a[i]);
+    }
+    return water_stored ;
     }
 };
