10b0652e9eb6213c1410e641f1a0f32e534eee02
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2025-07-28 23:15:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.6 MB (100.00%)

--- 0198-house-robber/solution.java
+++ 0198-house-robber/solution.java
@@ -1,22 +1,14 @@
 class Solution {
-
-    int[] dp = new int[107];
-
+    int[] dp ;
     public int rob(int[] nums) {
-       for(int i = 0 ; i < 107 ; i++) dp[i] = -1 ; 
-       int n = nums.length; 
-       return solve(nums,n-1); 
+        dp = new int[101];
+        for(int i = 0 ; i < 101 ; i++) dp[i] = -1 ;
+        return solve(nums,0);
     }
 
-    public int solve(int[] nums , int idx ){
-
-        if(idx < 0) return 0 ;
+    public int solve(int[] nums , int idx){
+        if(idx >= nums.length) return 0;
         if(dp[idx] != -1) return dp[idx];
-
-        int x = nums[idx] + solve(nums , idx - 2 );
-        int y = solve(nums , idx - 1) ;
-
-        return dp[idx] = Math.max(x,y);
+        return dp[idx] = Math.max( nums[idx] + solve(nums,idx + 2) , solve(nums,idx + 1));
     }
 }
-
