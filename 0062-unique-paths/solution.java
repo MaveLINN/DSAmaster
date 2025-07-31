12a50fcae6121efeaf7f8e99e5e941f6ad74eb3b
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2025-07-31 00:08:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.8 MB (99.98%)

--- 0062-unique-paths/solution.java
+++ 0062-unique-paths/solution.java
@@ -1,19 +1,20 @@
 class Solution {
-    private Integer[][] dp;
+    int[][] dp;
     public int uniquePaths(int m, int n) {
-        dp = new Integer[m][n];
+        dp = new int[m][n];
         for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ;j < n ; j++)
-               dp[i][j] = -1;
+            for(int j = 0 ; j < n ; j++){
+                dp[i][j] = -1;
+            }
         }
-        return solve(0,0,m,n);
+        return solve(m-1,n-1);
     }
 
-    public int solve(int i , int j , int m , int n){
+    public int solve(int i , int j){
+        if( i == 0 && j == 0) return 1 ;
+        if( i < 0 || j < 0 ) return 0 ;
 
-        if( i == m - 1 && j == n - 1) return 1 ;
-        if(i == m || j == n) return 0 ;
         if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = solve(i+1,j,m,n) + solve(i,j+1,m,n);
+        return dp[i][j] = solve(i - 1 , j ) + solve( i , j - 1) ;
     }
 }
