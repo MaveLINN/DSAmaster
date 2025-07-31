7c4aa3daef7fb9c940f6c58864738b98aae93a05
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2025-07-31 00:32:31 +0000

    Sync LeetCode submission Runtime - 2 ms (79.20%), Memory - 47.9 MB (99.72%)

--- 0064-minimum-path-sum/solution.java
+++ 0064-minimum-path-sum/solution.java
@@ -1,24 +1,22 @@
 class Solution {
-
-    int[][] dp = new int[201][201] ;
-    public int minPathSum(int[][] grid) {
-
-        int n = grid.length ;
-        int m = grid[0].length ;
-
-        for(int i = 0 ; i < 201 ; i++){
-            for(int j = 0 ; j < 201 ; j++)
-            dp[i][j] = -1 ;
+    int[][] dp;
+    public int minPathSum(int[][] a) {
+        int n = a.length ;
+        int m = a[0].length ;
+        dp = new int[n][m];
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                dp[i][j] = Integer.MAX_VALUE ;
+            }
         }
-        return solve(grid,n-1,m-1);
+        return solve(a,n-1,m-1);
     }
 
-    int solve(int[][] a , int n , int m){
+    public int solve(int[][] a , int i , int j ){
+        if(i < 0 || j < 0 ) return Integer.MAX_VALUE - 1 ;
+        if(i == 0 && j == 0) return a[i][j];
 
-        if(n == 0 && m == 0) return a[n][m] ;
-        if(n < 0 || m < 0) return 100000 ; // return max value when out of matrix
-
-        if(dp[n][m] != -1) return dp[n][m] ;
-        return dp[n][m] = a[n][m] + Math.min(solve(a,n-1,m),solve(a,n,m-1));
+        if(dp[i][j] != Integer.MAX_VALUE) return dp[i][j];
+        return dp[i][j] = a[i][j] + Math.min(solve(a,i,j-1) , solve(a,i-1,j) );
     }
 }
