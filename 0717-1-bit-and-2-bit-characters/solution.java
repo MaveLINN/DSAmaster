2e00f19f85ab0914cf7721d4fa95f417d189a1e1
GAURAV SINGH <30604921+MaveLINN@users.noreply.github.com>
2025-11-18 18:15:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.3 MB (63.48%)

---
+++ 0717-1-bit-and-2-bit-characters/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isOneBitCharacter(int[] bits) {
+        int i = 0 , n = bits.length ;
+        while(i < n - 1){
+            if(bits[i] == 0) i++;
+            else i+= 2;
+        }
+        if(i == n - 1) return true;
+        return false; 
+    }
+}
